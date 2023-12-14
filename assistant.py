974ff9ef778b2a96cea2bb318ebdbeb95cb86ff5
jeongesel <152484961+jeongesel@users.noreply.github.com>
2023-12-15 04:56:51 +0900

    Create assistant.py

---
+++ assistant.py
@@ -0,0 +1,15 @@
+import os
+import json
+import time
+from openai import OpenAI
+from dotenv import load_dotenv
+from collection import OrderedDict
+
+def callchat(message):
+  load_dotenv()
+  client = OpenAI(
+  api_key=os.environ['OPENAI_API_KEY'],
+  )
+  #스레드 생성, 연결(runs)
+  run = client.beta.threads.create_and_run(
+  assistant_id="asst_FP
